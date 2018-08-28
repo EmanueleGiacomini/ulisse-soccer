dfe90098bed4f861e47c7b71c211799194f0804f
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2018-08-28 10:34:34 +0200

    Makefile finally works
    Now heading to important stuff

--- makefiles/teensy-makefile-test/hello_world/src/main.c
+++ makefiles/teensy-makefile-test/hello_world/src/main.c
@@ -1,22 +1,37 @@
+#include "kinetis.h"
 #include <stdlib.h>
-#include <Arduino.h>
 #include "queue.h"
 #include "foo.h"
 
-void setup() {
-  int i;
-  for(i=2;i<32;++i)
-    pinMode(i, 1);
-    digitalWrite(i, 1);
-}
-
-void loop() {
-
-}
 // pin 13: PTC5
 
+#define OF_VALUE 1000000
+
+void setLed(int state) {
+  if(state) {
+    GPIOC_PTOR |= 1 << 5;
+  } else {
+    GPIOC_PTOR &= ~(1<<5);
+  }
+}
+
 int main() {
-  setup();
-  while(1)
-    loop();
+  // Set LED pin as an output
+  PORTC_PCR5 = PORT_PCR_SRE | PORT_PCR_DSE | PORT_PCR_MUX(1);
+  GPIOC_PDDR |= 1 << 5;
+  uint32_t _counter = 0;
+  uint32_t counter = 0;
+  uint8_t state = 0;
+
+  uint8_t* ptr = (uint8_t*)calloc(10, sizeof(uint8_t));
+  while(1) {
+    if(++_counter > OF_VALUE) {
+      ++counter;
+      if(counter > 1000000) {
+        counter = 0;
+        state = !state;
+        setLed(state);
+      }      
+    }    
+  }
 }
