58195061a9d46750d651e1c643b5f6fdd18d8fcb
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2018-09-04 09:27:18 +0200

    Add systime_init and usb_init func

--- src/arch/teensy/uart.c
+++ src/arch/teensy/uart.c
@@ -2,6 +2,7 @@
 #include "uart.h"
 
 #define UART_BUFFER_SIZE 128
+#define SYSCLK 30
 
 void setBaud115200(void) {
 #define BAUD 115200
@@ -26,18 +27,93 @@ typedef struct UART {
 
 static UART uart_0;
 
+void usb_init() {
+  //* Setup USB device *//
+  
+  // assume 48 MHz clock already running
+  SIM_SCGC4 |= SIM_SCGC4_USBOTG;
+
+  // Clear all ISR flags
+  USB0_ISTAT = 0xFF;
+  USB0_ERRSTAT = 0xFF;
+  USB0_OTGISTAT = 0xFF;
+
+  // enable USB
+  USB0_CTL = USB_CTL_USBENSOFEN;
+  USB0_USBCTRL = 0;
+
+  // enable reset interrupt
+  USB0_INTEN = USB_INTEN_USBRSTEN;
+  USB0_CONTROL = USB_CONTROL_DPPULLUPNONOTG;
+  
+  /**
+  SIM_SCGC4 |= SIM_SCGC4_USBOTG;
+  PORTB_PCR0 |= 0 | PORT_PCR_MUX(1);  // PTB0 as I/O pin
+  // SIM Configuration
+  SIM_SCGC4|=(SIM_SCGC4_USBOTG); // USB Clock Gating
+  SIM_SCGC6|=(SIM_SCGC4_USBOTG); // USB Clock Gating
+
+  // USB pre-initialization 
+  USB0_USBTRC0 |= USB_USBTRC_USBRESET;
+  while(USB0_USBTRC0 & USB_USBTRC_USBRESET);  // Wait for reset to happen
+  USB0_ISTAT |= USB_ISTAT_USBRST;
+  // Enable USB Reset Interrupt
+  USB0_INTEN |= USB_INTEN_USBRSTEN; //USBRST Interrupt Enable
+  USB0_USBCTRL = 0x00;
+  //USB0_USBTRC0 |= 0x40; bit not documented :(
+  USB0_CTL |= USB_CTL_USBENSOFEN;
+  **/
+}
 
 struct UART* UART_init(const char* device __attribute__((unused)), uint32_t baud) {
+  uint16_t ubd, brfa;
+  uint16_t temp;
+
+  // Configure UART struct
   UART* uart=&uart_0;
   uart->uart_num=0;
 
-  switch(baud) {
-    case 115200: setBaud
-  }
+  // Enable UART pins by configuring PORTx_PCRn register
+  // UART0 ->  PORTA_PCR0 (Uart0_rx) mux(2)
+  // UART0 ->  PORTA_PCR1 (Uart0_tx) mux(2)
 
-  // Enable clock
+  usb_init();
+
+  // rx: PullUp Enable to keep it on 3.3V 
+  PORTA_PCR0 |= PORT_PCR_MUX(2) | PORT_PCR_PE;
+  // tx: SlewRate Enable, Drive Strenght Enable
+  PORTA_PCR1 |= PORT_PCR_MUX(2) | PORT_PCR_SRE | PORT_PCR_DSE;
+
+  // Enable the clock on UART0
   SIM_SCGC4 |= SIM_SCGC4_UART0;
-  
+  // Disable Receiver and Transmitter while Uart is begin initialized
+  UART0_C2 &= ~(UART_C2_RE | UART_C2_TE);
 
+  // Configure the UART for 8-bit mode, no parity
+  // We need all default settings, so entire register is cleared
+  UART0_C1 = 0;
 
+  // Calculate baud settings
+  ubd = (uint16_t)((SYSCLK*1000)/(baud * 16));
+
+  // Save off the current value of the UARTx_BDH except for the SBR (first 5 bits)
+  temp = UART0_BDH & ~(0x1F);
+  UART0_BDH = temp | ((ubd & 0x1F00) >> 8);
+  UART0_BDL = (uint8_t) (ubd & 0xFF);
+
+  // Determine if a fractional divider is needed to get closer to the baud rate
+  brfa = (((SYSCLK*32000)/(baud * 16)) - (ubd * 32));
+
+  // Save off the current value of UART0_C4 except for BRFA (BaudRateFineAdjust)
+  temp = UART0_C4 & ~(0x1F);
+  UART0_C4 = temp | UART_C4_BRFA(brfa);
+
+  // Enable transmitter and receiver
+  UART0_C2 |= UART_C2_RE | UART_C2_TE;
+  return &uart_0;
+}
+
+uint8_t UART_getChar(struct UART* uart __attribute__((unused))) {
+  while(!(UART0_S1 & UART_S1_RDRF));
+  return UART0_D;
 }
--- src/micro_test/digio_test.c
+++ src/micro_test/digio_test.c
@@ -1,6 +1,7 @@
 #include <kinetis.h>
 #include "digio.h"
 #include "eeprom.h"
+#include "uart.h"
 
 #define TO1 9000000
 #define TO2 5000000
@@ -13,49 +14,99 @@ void ledToggle(int state) {
   DigIO_setValue(13, state);
 }
 
-int main(void) {
-  uint32_t ctr1 = 0, ctr2 = 0, ctr3 = 0;
-  uint8_t temp = 0;
-  //uint8_t state = 0b11010101;
+void systime_init() {
+  // 16pF and 8pF load capacitors active
+  OSC0_CR = OSC_SC16P | OSC_SC8P;
+  // Enabling the oscillator for 8 MHz crystal
+  MCG_C2 = MCG_C2_RANGE0(1) | MCG_C2_HGO0 | MCG_C2_EREFS;
 
-  // Set pin 13
+  // Select ext oscillator
+  MCG_C1 = MCG_C1_CLKS(2) | MCG_C1_FRDIV(3);
+
+  // wait for oscillator to initialize
+  ledToggle(1);
+  while(!(MCG_S & MCG_S_OSCINIT0)) {}
+  ledToggle(0);
+
+  // wait for Reference clock to switch to external reference
+  ledToggle(1);
+  while(MCG_S & MCG_S_IREFST) {}
+  ledToggle(0);
+
+  // wait for MCGOUT to switch over to the external reference clock
+  ledToggle(1);
+  while(((MCG_S & MCG_S_CLKST_MASK) >> 2) != 0x2) {}
+  ledToggle(0);
+
+  // now configure the PLL and move to PBE mode
+  MCG_C5 = MCG_C5_PRDIV0(1);
+
+  // Set the VDIV field to 0 which is x24, giving 4x24 = 96 MHz
+  MCG_C6 = MCG_C6_CME0 | MCG_C6_PLLS;
+
+  // wait until the source of the PLLS clock has switched to the PLL
+  ledToggle(1);
+  while(!(MCG_S & MCG_S_PLLST)) {}
+  ledToggle(0);
+
+  // wait until the PLL has achieved lock
+  while(!(MCG_S & MCG_S_LOCK0)) {}
+
+  // set up the SIM clock dividers BEFORE switching to the PLL to ensure
+  // the system clock speeds are in specs
+  SIM_CLKDIV1 = SIM_CLKDIV1_OUTDIV1(0) | SIM_CLKDIV1_OUTDIV2(1)
+              | SIM_CLKDIV1_OUTDIV3(1) | SIM_CLKDIV1_OUTDIV4(3);
+
+  // Transition into PEE by setting CLKS to 0
+  MCG_C1 &= ~0xC0;
+  
+  // Wait for MCGOUT to switch over to the PLL
+  ledToggle(1);
+  while (((MCG_S & MCG_S_CLKST_MASK) >> 2) != 0x3){}
+  ledToggle(0);
+
+  // The USB clock divider in the System Clock Divider Reg 2
+  // Should be configured to generate the 48 MHz clock
+
+  SIM_CLKDIV2 |= SIM_CLKDIV2_USBDIV(1);
+  
+}
+
+void _usb_init() {
+  //* Setup USB device *//
+  // assume 48 MHz clock already running
+  SIM_SCGC4 |= SIM_SCGC4_USBOTG;
+
+  // Clear all ISR flags
+  USB0_ISTAT = 0xFF;
+  USB0_ERRSTAT = 0xFF;
+  USB0_OTGISTAT = 0xFF;
+
+  // enable USB
+  USB0_CTL = USB_CTL_USBENSOFEN;
+  USB0_USBCTRL = 0;
+
+  // enable reset interrupt
+  USB0_INTEN = USB_INTEN_USBRSTEN;
+  USB0_CONTROL = USB_CONTROL_DPPULLUPNONOTG;
+}
+
+int main(void) {
   PORTC_PCR5 |= PORT_PCR_SRE | PORT_PCR_DSE | PORT_PCR_MUX(1);
   DigIO_setDirection(13, Output);
-
-  // Initialize EEPROM
-  EEPROM_init();
+  ledToggle(1);
+  systime_init();
   
+  _usb_init();
+  UART_init("", 115200);
+  ledToggle(0);
   
-  uint8_t state = 0;
-  EEPROM_write(LED_OFFSET, &state, sizeof(uint8_t));
-  
-  state = 0;
-  EEPROM_read(&state, LED_OFFSET, sizeof(uint8_t));
 
-  ledToggle(state);
 
-  /*
-  while(1) {
-    ctr1++;
-    if(ctr1 > TO1) {
-      ctr1=0;
-      ctr2++;
-      if(ctr2 > TO2) {
-        ctr2=0;
-        ctr3++;
-        if(ctr3 > TO3) {
-          ctr3=0;
-          int _state = state >> temp;
-          ledToggle(_state & 0x1);
-          temp += 1;
-          if(temp >= 8)
-            break;
-        }        
-      }
-    }    
-  } 
-  */   
-
+  //UART_init("", 115200);
+  //ledToggle(1);
+  //uint8_t recv=UART_getChar(NULL);
+  //ledToggle(recv);
 
   /*
   /// Led blink
